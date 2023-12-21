c9d0bd9839dcbc8cfacb7a922dfdd7b619284575
bronaKeevers <20098883@mail.wit.ie>
2023-12-21 15:03:36 +0000

    Topic 12 - Get Movies from API

--- movies-api/index.js
+++ movies-api/index.js
@@ -12,8 +12,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT; 
 
-app.use('/api/movies',authenticate,  moviesRouter);
 app.use(cors());
+
+app.use('/api/movies',authenticate,  moviesRouter);
+
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
